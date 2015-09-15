f6f3d0c32bee6b2d3f450a223d5d401f8a54fb25
git <wangjian52817@sina.com>
2015-09-15 21:19:25 +0800

    init project

--- index.php
+++ index.php
@@ -1,1 +1,8 @@
-asdada
+<?php
+
+define('APPLICATION_PATH', dirname(__FILE__));
+
+$application = new Yaf_Application( APPLICATION_PATH . "/conf/application.ini");
+
+$application->bootstrap()->run();
+?>
